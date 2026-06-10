e44077394e9ca29614b9e69a3fe29b879436a8cb
erik <erik@erik.xyz>
2026-06-10 17:08:40 +0800

    更新错误

--- admin/support/helpers.php
+++ admin/support/helpers.php
@@ -3,6 +3,8 @@
  * Copyright (c) 2026 erik <erik@erik.xyz> — https://erik.xyz
  */
 
+use Erikwang2013\Jwt\JWT;
+use Erikwang2013\Jwt\JWTFactory;
 use Illuminate\Translation\ArrayLoader;
 use Illuminate\Translation\Translator;
 use Illuminate\Validation\Factory;
@@ -17,4 +19,17 @@ function validator(array $data, array $rules, array $messages = [], array $attri
         $factory = new Factory(new Translator(new ArrayLoader(), 'en'));
     }
     return $factory->make($data, $rules, $messages, $attributes);
+}
+
+/**
+ * JWT 便捷包装
+ */
+function jwt(): \Erikwang2013\Jwt\JwtWrapper
+{
+    static $wrapper = null;
+    if ($wrapper === null) {
+        $jwt = JWTFactory::createFromConfig(config('plugin.erikwang2013.jwt.jwt'));
+        $wrapper = new \Erikwang2013\Jwt\JwtWrapper($jwt);
+    }
+    return $wrapper;
 }
